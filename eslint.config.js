4408c0cfec5469ebebfa1ce35d374f49a4507be7
AndjelaBabic <andjelasbabic@gmail.com>
2024-12-31 17:42:34 +0100

    Added turborepo to the project

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,14 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
+import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    ignores: ["**/node_modules/", ".git/", "eslintrc.js"],
+  },
 ];
---
+++ estlintrc.js
@@ -0,0 +1,7 @@
+export default {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+};
